3db15e0f33181bd70e0f0f682d7d58da83680aca
Liudmyla Chursinova <lyubahalik@gmail.com>
2026-06-30 02:39:49 +0200

    HW7 JS All tasks

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+function hahdleColorChange() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+}
+
+changeColorBtn.addEventListener('click', hahdleColorChange);
